c0ddcf7a2a80e7c31a6475dc15ecaefa753021ea
Reece Williams <reecepbcups@gmail.com>
2023-07-04 17:39:45 -0500

    favicon cache

--- main.go
+++ main.go
@@ -60,6 +60,7 @@ var (
 		"/unsubscribe":          true,
 		"/unsubscribe_all":      true,
 		"/validators":           true,
+		"/websocket":            true,
 	}
 
 	DefaultCacheTimeSeconds = 6
@@ -206,6 +207,25 @@ func main() {
 	// panic | crash
 	r.HandleFunc("/panic", func(w http.ResponseWriter, r *http.Request) {
 		panic("Panic!")
+	})
+
+	r.HandleFunc("/favicon.ico", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "image/x-icon")
+
+		if res := cache.Get("favicon.ico"); res != nil {
+			fmt.Println("Favicon cache hit")
+			stats["CACHE_favicon"]++
+			fmt.Fprint(w, res.Value)
+			return
+		}
+
+		body, err := ioutil.ReadFile("static/favicon.png")
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		cache.Set("favicon.ico", string(body), int(30*time.Minute.Seconds()))
+		fmt.Fprint(w, string(body))
 	})
 
 	r.HandleFunc("/stats", func(w http.ResponseWriter, r *http.Request) {
